758bf55a0f881ac8bcd6f990c21d55f3bc88591e
Deepak joshi <deepak10995@gmail.com>
2020-08-20 21:59:17 +0530

    closest value done

--- BST_closestValue.py
+++ BST_closestValue.py
@@ -2,8 +2,37 @@
 # Worst: time complexity: O(n) | Space Complexity: O(n)
 
 def findClosestValueInBst(tree,target):
-    return findClosestValueInBstHelper(tree, target);
+    return findClosestValueInBstHelper(tree, target,float("inf"))
 
 def findClosestValueInBstHelper(tree,target,closest):
     if tree is None:
-        return closest;
+        return closest
+    if abs(target - closest) > abs(target - tree.value):
+        closest = tree.value
+    if target < tree.value:
+        return findClosestValueInBstHelper(tree.left,target, closest)
+    elif target > tree.value:
+        return findClosestValueInBstHelper(tree.right,target, closest)
+    else :
+        return closest
+
+# Average : time complexity: O(log(n)) | space complexity: O(1)
+# Worst: time complexity: O(n) | Space Complexity: O(1)
+
+# tierative method without recursion
+
+def findClosestValueInBst1(tree,target):
+    return findClosestValueInBstHelper(tree, target,float("inf"))
+
+def findClosestValueInBstHelper1(tree,target,closest):
+    currentNode = tree
+    while currentNode is not None:
+        if abs(target - closest) > abs(target - tree.value):
+            closest = currentNode.value
+        if target < tree.value:
+            currentNode = currentNode.left
+        elif target > tree.value:
+            currentNode = currentNode.right
+        else :
+            break
+    return closest
