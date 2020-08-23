d62c90af9126a6e15272bcc59f1ff66c3cdb8f9b
Deepak joshi <deepak10995@gmail.com>
2020-08-24 02:41:43 +0530

    adding branch nodes and pushing/appendingeach branch sums in an array

---
+++ BT_BranchSums.py
@@ -0,0 +1,22 @@
+class BinaryTree:
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def BT_BranchSums(root):
+    sums = []
+    calculateBT_BranchSums(root,0,sums)
+    return sums
+
+def calculateBT_BranchSums(node,runningSum,sums):
+    if node is None:
+        return
+
+    newRunningSum = runningSum + node.value
+    if node.left is None and node.right is None:
+        sums.append(newRunningSum)
+        return
+
+    calculateBT_BranchSums(node.left, newRunningSum,sums)
+    calculateBT_BranchSums(node.right,newRunningSum,sums)
